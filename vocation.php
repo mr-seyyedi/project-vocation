5d75de8eed03b640c0232c7314dc2e8bfd409a82
mr-seyyedi <ironheart0944@gmail.com>
2022-09-21 09:36:31 +0430

    vocations

--- vocation.php
+++ vocation.php
@@ -2,6 +2,33 @@
     require_once "db.php";
     if (isset ($_POST['vocation'])){ header("Location:vacation.php"); }
     if (isset ($_POST['logaut'])){ header("Location:logaut.php"); }
+    if (isset ($_POST['save'])){
+      if (empty ($_POST['date'])){
+        echo("<script>alert('null date!! ')</script>");
+      }
+      elseif(empty($_POST['time'])){
+        echo("<script>alert('null time!! ')</script>");
+      }
+      elseif(empty($_POST['desc'])){
+        echo("<script>alert('null desc!! ')</script>");
+      }
+      else{
+        
+        $id=$_SESSION['userid'];
+        $date=$_POST['date'];
+        $time=$_POST['time'];
+        $desc=$_POST['desc'];
+        $sql="INSERT INTO `vocations`(`date`, `desc`, `time`, `userid`) VALUES ( ? , ? , ? , ?)";
+        $stmt=$db->prepare($sql);
+        $stmt->bindvalue(1 , $date);
+        $stmt->bindvalue(2 , $desc);
+        $stmt->bindvalue(3 , $time);
+        $stmt->bindvalue(4 , $id);
+        $stmt->execute();
+        echo("<script>alert('save ok!! ')</script>");
+      }
+    }
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -21,6 +48,7 @@
         $stmt->execute();
         foreach ($stmt as $row){
             $id=$row['id'];
+            $_SESSION['userid']=$id;
             $name=$row['name'];
             $family=$row['family'];
 ?>
@@ -38,10 +66,40 @@
     <hr>
 </div>
 <?php } } ?>
-
 <h2>لیست مرخصی ها</h2>
 <hr>
 <h2>ثبت مرخصی ها</h2>
+<br><br>
+
+<form action="#" method="POST">
+  <div class="form-group">
+  <div class="form-row">
+    <div class="col">
+      <label for="date"><b>تاریخ</b></label>
+      <input type="text" class="form-control" id="date" name="date" value="<?php print date("Y/m/d") ?>">
+    </div>
+    <div class="col">
+      <label for="time"><b>زمان</b></label>
+      <select class="form-control" id="time" name="time">
+        <option value="1day">یک روزه</option>
+        <option value="2day">دو روزه</option>
+        <option value="3day">سه روزه</option>
+        <option value="4day">چهار روزه</option>
+      </select>
+    </div>
+  </div>
+  </div>
+  <div class="form-group">
+    <label for="desc"><b>شرح</b></label>
+    <textarea class="form-control" id="desc" rows="4" name="desc"></textarea>
+  </div>
+  <button type="submit" class="btn btn-secondary btn-lg btn-block" name="save">ذخیره</button>
+</form>
+
+
+
+
+
 
 
 <script src="asset/js/jquery.min.js"></script>
